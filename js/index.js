c360b886b061f51226726ccc856d0b6bd7c2d2c2
QuangVu-dev <tqvu97@gmail.com>
2024-04-29 21:27:24 +0700

    final version

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("site-header").style.height = "70px";
   } else {
     document.getElementById("site-header").style.height = "90px";
@@ -12,7 +12,7 @@ window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("navigation").style.padding = "0.5rem 1rem";
     document.getElementById("site-header").style.boxShadow =
       "0px 3px 6px 0px rgba(0, 0, 0, 0.05";
@@ -24,8 +24,3 @@ function scrollFunction() {
       "transparent";
   }
 }
-
-// Dark mode
-document.getElementById("switchButton").onclick = function () {
-  document.getElementById("myBody").classList.toggle("dark");
-};
